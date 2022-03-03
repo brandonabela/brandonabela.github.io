9aa4bf774496ccede9440b4e8046e4d9ee9fcec0
Brandon Abela <brandonabela9@gmail.com>
2022-03-03 23:26:35 +0100

    Add Contact Page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { Button, Col, Form, Row } from 'react-bootstrap';
 
+import './Contact.scss';
+import profile from '../data/profile.json';
 import Navigation from '../components/Navigation';
 
 
@@ -9,6 +12,32 @@ function Contact() {
       <Navigation />
 
       <h1 className="page-header"> Contact </h1>
+
+      <h2> Contact Form </h2>
+
+      <form action={"https://formspree.io/f/" + profile.formspreeId} method="POST">
+        <Row>
+          <Col sm={6}>
+            <Form.Control className="mb-3" type="text" name="name" placeholder="Full Name" required />
+          </Col>
+
+          <Col sm={6}>
+            <Form.Control className="mb-3" type="text" name="subject" placeholder="Subject" required />
+          </Col>
+
+          <Col sm={12}>
+            <Form.Control className="mb-3" type="email" name="email" placeholder="Email Address" required />
+          </Col>
+
+          <Col sm={12}>
+            <Form.Control className="mb-3" as="textarea" name="message" placeholder="Your message" rows={3} required />
+          </Col>
+        </Row>
+
+        <div className="d-flex flex-row-reverse">
+          <Button className="box" type="submit"> <i className="bi bi-send-fill"></i> Send Message </Button>
+        </div>
+      </form>
     </div>
   );
 }
