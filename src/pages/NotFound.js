d0ac4f75a6f3db89f43b6b80e005e589afc3116b
Brandon Abela <brandonabela9@gmail.com>
2022-03-08 16:13:13 +0100

    Add Not Found Page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,14 +1,28 @@
 import React from 'react';
-import { Container, Row } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+import './NotFound.scss';
+import Navigation from '../components/Navigation';
 
 
 function NotFound() {
   return (
-    <Container>
-      <Row className="text-center">
-        <p> Not Found </p>
-      </Row>
-    </Container>
+    <div className="not-found box-outer">
+      <Navigation />
+
+      <h1 className="page-header"> Not Found </h1>
+
+      <div className="text-center">
+        <h1 className="text-404"> 404 </h1>
+
+        <p className="text-desc"> Oops, Something Went Wrong </p>
+
+        <Button className="box" as={Link} to="/"> <i className="bi bi-house-fill"></i> Back to Home </Button>
+      </div>
+
+
+    </div>
   );
 }
 
